f344c2acc81a81d2b104f892e63ef24550465082
smv7 <smv7.github@gmail.com>
2022-08-12 01:47:12 -0400

    Adding method to extract paragraph and list items data as single list for features with list-based format

--- src/job_technology_scraper/job_technology_scraper/pipelines.py
+++ src/job_technology_scraper/job_technology_scraper/pipelines.py
@@ -14,18 +14,37 @@ class PythonJobPipeline:
     '''
     Class to do data preparation on PythonJob objects.'''
 
+
+    def _extract_paragraphs_and_list_items(self, section: str) -> list[str]:
+        '''
+        Return a list with the content of all paragraph and list item tags in section.'''
+
+        soup = BS(section, 'html.parser')
+
+        # Extracting and concatenating all lists into a single one.
+        lists = soup.find_all('ul')
+        lists_joined = []
+        for i in range(len(lists)):
+            lists_joined.extend(lists[i])
+
+        paragraph_content = [p.text for p in lists.find_all('p')]
+        lists_content = [li.text for li in lists_joined]
+        full_data: list[str] = paragraph_content + lists_content
+
+        return full_data
+
+
     def process_item(self, item: PythonJobItem, spider):
         # NOTE: job_title is comes already cleaned.
+
         # Processing job_description.
         clean_description: str = BS(item['job_description'], 'html.parser').text.replace('\n', ' ').strip()
+
         # Processing job_restrictions:
         clean_restrictions: str = BS(item['job_restrictions', 'html.parser']).text.replace('\n', ' ').strip()
+
         # Processing job_requirements:
-        reqs_soup = BS(item['job_requirements'], 'html.parser')
-        reqs_lists = reqs_soup.find_all('ul')
-        reqs1 = [req.text for req in reqs_soup.find_all('p')]
-        reqs2 = [req.text for req in reqs_lists[0] + reqs_lists[1]]
-        clean_requirements: list[str] = reqs1 + reqs2
+        clean_requirements: list[str] = self._extract_paragraphs_and_list_items(item['job_requirements'])
         # TODO: Processing company_about:
         # TODO: Processing contact_info:
 
